c8c32b37607976ccf3ea2e74820146401ddbed42
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-26 11:49:09 +0300

    added login system

--- register.php
+++ register.php
@@ -35,8 +35,7 @@ if (isset($_POST['submit'])) {
 
         $SORGU->execute();
         //!Kayıt başarılıysa login sayfasına yönlendir
-        /*  header("location: login.php"); */
-        echo "kayıt başrılı";
+        header("location: login.php");
     }
 }
 ?>
@@ -103,5 +102,7 @@ if (!empty($errors)) {
 </div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="./public/js/hideShow.js"></script>
+    <script src="./public/js/autoCloseAlert.js"></script>
   </body>
 </html>
