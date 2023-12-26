c4b7d7d53f02ded883f2f01e96594ca8e978ec02
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-26 14:48:43 +0300

    fixed navbar

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <nav class="navbar navbar-expand-lg bg-primary " data-bs-theme="dark">
   <div class="container-fluid ">
-    <a class="navbar-brand " href="index.php">Customer Cari</a>
+    <a class="navbar-brand " href="index.php">School Management System</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -18,7 +18,14 @@ session_start();
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Profile
+            <img
+            src="./IMG_20231009_194219.jpg"
+            class="rounded-circle"
+            height="25"
+
+            alt="Black and White Portrait of a Man"
+            loading="lazy"
+          />
           </a>
           <ul class="dropdown-menu  dropdown-menu-end">
             <li><a class="dropdown-item" href="#">Action</a></li>
