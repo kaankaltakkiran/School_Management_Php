17c0704bf334b277f7c9d5ad1c32dab66142f35f
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-26 15:29:59 +0300

    added add: teacher,admin,student index page card

--- index.php
+++ index.php
@@ -16,10 +16,48 @@ $activePage = 'index';
   <div class="container">
       <div class="row">
       <h1 class="text-danger text-center mt-3">School Management System V1</h1>
-        <h2 class="text-warning text-center mt-3">Welcome</h2>
-        <h3 class="text-info text-center mt-3">Name: <?php echo $_SESSION['adsoyad'] ?></h3>
+      <?php if ($_SESSION['rol'] == 1) {?>
+        <h2 class="text-success text-center mt-3">This İs Admin Page</h2>
+        <?php }?>
+
+        <h3 class="text-warning text-center mt-3">Welcome</h3>
+        <h4 class="text-info text-center mt-3">Name: <?php echo $_SESSION['adsoyad'] ?></h4>
+      </div>
+      <?php if ($_SESSION['rol'] == 1) {?>
+      <div class="row">
+        <div class="col-md-4">
+        <div class="card m-3 " style="width: 18rem;">
+  <img src="./IMG_20231009_194219.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-danger">Admin</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Add Admin</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4">
+        <div class="card m-3" style="width: 18rem;">
+  <img src="./IMG_20231009_194219.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-danger">Teacher</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Add Teacher</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4">
+        <div class="card m-3" style="width: 18rem;">
+  <img src="./IMG_20231009_194219.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-danger">Student</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add Student</a>
+  </div>
+</div>
+        </div>
       </div>
   </div>
+  <?php }?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
